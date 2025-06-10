36741aa89bb6541782e663fcb76212f094d0dadf
caiosmsb <caiosampaioms@gmail.com>
2025-06-10 00:23:42 -0300

    sistema completo

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SistemaLocacao from './SistemaLocacao'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SistemaLocacao />
+  </React.StrictMode>
+)
